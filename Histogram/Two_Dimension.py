34ff56dd1e51490ee5a27a50823ef3852f37e6f4
BitRebels <122071878+mohd-shaamii@users.noreply.github.com>
2024-01-24 09:53:59 +0530

    Update and rename two_dimension.py to Two_Dimension.py

--- Histogram/Two_Dimension.py
+++ Histogram/Two_Dimension.py
@@ -33,6 +33,6 @@ plt.scatter(xs, ys1, marker='.', color='black', label='ys1')
 plt.scatter(xs, ys2, marker='.', color='gray', label='ys2')
 plt.xlabel('xs')
 plt.ylabel('ys')
-plt.legend(loc='upper right')  # Change the legend location if needed
+plt.legend(loc=9) 
 plt.title("Very Different Joint Distributions")
 plt.show()
